5073fe2e6af9297c2a6344cd2aa8635e71aff622
name1807 <111066357+name1807@users.noreply.github.com>
2025-02-13 08:32:32 +0700

    add

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import pandas as pd
+from selenium import webdriver
 
-df = pd.read_csv(r"C:\Users\name1\OneDrive\Desktop\raw_data_product_2025-02-12T23_37_26.xlsx")
+file_path = input("path excel")
 
-print(df)
+format_path = r"{}".format(file_path).replace('"', ' ')
+
+df = pd.read_excel(r"C:\Users\name1\Downloads\raw_data_2025-02-13T00_41_08.xlsx", header=0)
+
+filtered_df = df[df['price'] > 100]
+
+link_column = df['product_link'].to_list();
+
+for i in link_column:
+  print(link_column[i])
